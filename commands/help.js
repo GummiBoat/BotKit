fa0bc51d36bdc795c02e15dfd18ea1c179ab0d03
GummiBoat <huppertzraphael@gmail.com>
2018-11-21 23:21:51 +0100

    help and rewritten profile

---
+++ commands/help.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args, config) => {
+	message.channel.send('**BotKit Help:**\n`.verify` - Verifies your account\n`.profile` - Shows the profile of a user. Only available for Moderators.');
+};
--- commands/profile.js
+++ commands/profile.js
@@ -1,10 +1,34 @@
 exports.run = (client, message, args) => {
+  var guilds = client.guilds.get('424539676389408779');
+  var member = guilds.members.get(message.author.id);
+  if(!member.roles.has('424545380932517888')){
+	  message.reply('you are not a moderator! >:(');
+  }
   if (args.length == 0){
     let user = client.getUser.get(message.author.id);
     if(!user)
       message.reply('you are not verified yet.');
     else
-      message.channel.send(`\`\`\`\nDiscord ID: ${user.discord_id}\nGamekit ID: ${user.gamekit_id}\nVerified: ${user.verified}\`\`\``);
+    	message.channel.send({embed: {
+    		color: 4273849,
+    		author: {
+    			name: message.author.username,
+    			icon_url: message.author.avatarURL
+    		},
+    		fields: [{
+    			name: "Gamekit URL",
+    			value: "https://gamekit.com/profil/"+user.gamekit_id+"/"
+    		},
+    		{
+    			name: "Discord User",
+    			value: "<@"+user.discord_id+">"
+    		}],
+    		timestamp: new Date(),
+    		footer: {
+    			icon_url: client.user.avatarURL,
+    			text: "(c) BotKit"
+    		}
+    	}});
   } else {
     let member = message.mentions.members.first();
     if(member){
@@ -12,7 +36,26 @@ exports.run = (client, message, args) => {
       if(!user)
         message.reply('that user is not verified yet.');
       else
-        message.channel.send(`\`\`\`\nDiscord ID: ${user.discord_id}\nGamekit ID: ${user.gamekit_id}\nVerified: ${user.verified}\`\`\``);
+      	message.channel.send({embed: {
+      		color: 4273849,
+      		author: {
+      			name: user.username,
+      			icon_url: user.avatarURL
+      		},
+      		fields: [{
+      			name: "Gamekit URL",
+      			value: "https://gamekit.com/profil/"+user.gamekit_id+"/"
+      		},
+      		{
+      			name: "Discord User",
+      			value: "<@"+user.discord_id+">"
+      		}],
+      		timestamp: new Date(),
+      		footer: {
+      			icon_url: client.user.avatarURL,
+      			text: "(c) BotKit"
+      		}
+      	}});
     } else {
       message.reply('that is not a valid mention.');
     }
